ec365177b5c71395ee6c14e8562d64ec7281855f
hkas <kasshawa@gmail.com>
2019-08-26 15:58:07 +0000

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'posts';
+  // title = 'posts';
+  posts = [
+    {
+      title: 'Mon premier post',
+      content: 'Mon premier post dans le premier blog',
+      loveIts: 3,
+      createdAt: new Date()
+    },
+    {
+      title: 'Mon 2eme post',
+      content: 'Mon 2eme post dans le 2eme blog',
+      loveIts: -1,
+      createdAt: new Date()
+    },
+    {
+      title: 'Mon 3eme post',
+      content: 'Mon 3eme post dans le 3eme blog',
+      loveIts: 0,
+      createdAt: new Date()
+    },
+  ];
 }
